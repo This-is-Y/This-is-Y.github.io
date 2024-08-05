920481f6fb297deaa51790d004b6f32f4d3d8340
This_is_Y <2317210636@qq.com>
2024-08-05 19:17:07 +0800

    Site updated: 2024-08-05 19:17:03

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -8,7 +8,7 @@ KEEP.initModeToggle = () => {
     enableLightMode() {
       document.documentElement.classList.remove('dark-mode')
       document.documentElement.classList.add('light-mode')
-      this.iconDom && (this.iconDom.className = 'fas fa-moon')
+      this.iconDom.className = 'fas fa-moon'
       KEEP.themeInfo.styleStatus.isDark = false
       KEEP.setStyleStatus()
     },
@@ -16,7 +16,7 @@ KEEP.initModeToggle = () => {
     enableDarkMode() {
       document.documentElement.classList.add('dark-mode')
       document.documentElement.classList.remove('light-mode')
-      this.iconDom && (this.iconDom.className = 'fas fa-sun')
+      this.iconDom.className = 'fas fa-sun'
       KEEP.themeInfo.styleStatus.isDark = true
       KEEP.setStyleStatus()
     },
@@ -48,13 +48,10 @@ KEEP.initModeToggle = () => {
     },
 
     initModeToggleButton() {
-      if (this.themeModeToggleBtn && !this.themeModeToggleBtn?.hasClickListener) {
-        this.themeModeToggleBtn.addEventListener('click', () => {
-          const isDark = document.documentElement.classList.contains('dark-mode')
-          isDark ? this.enableLightMode() : this.enableDarkMode()
-        })
-        this.themeModeToggleBtn.hasClickListener = true
-      }
+      this.themeModeToggleBtn.addEventListener('click', () => {
+        const isDark = document.documentElement.classList.contains('dark-mode')
+        isDark ? this.enableLightMode() : this.enableDarkMode()
+      })
     },
 
     initModeAutoTrigger() {
